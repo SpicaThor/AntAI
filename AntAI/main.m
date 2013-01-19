bfa98861c4e44e1d55c94d0207b304937b3e36f7
Ariel <ariel.tkachenko@moovitapp.com>
2013-01-19 16:57:25 +0200

    Initial Commit

---
+++ AntAI/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AntAI
+//
+//  Created by Ariel Tkachenko on 1/19/13.
+//  Copyright (c) 2013 Thor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
